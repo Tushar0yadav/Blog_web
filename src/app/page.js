f33eb46f595df35a0daaafb2b604ea708bbe44ac
Tushar0yadav <126134177+Tushar0yadav@users.noreply.github.com>
2023-09-18 20:51:52 +0530

    '

--- src/app/page.js
+++ src/app/page.js
@@ -25,9 +25,9 @@ export default async function Home() {
                 </svg>
                 <p className="leading-relaxed mb-6">
                   Holden grows throughout the book from a somewhat immature kid
-                  to a mature, wise, grownup boy who realizes that taking care
+                  to a mature wise grownup boy who realizes that taking care
                   of his sister is more important than getting yelled at by his
-                  parents for getting kicked out of another school.
+                  parents for getting kicked out of another school
                 </p>
                 <a className="inline-flex items-center">
                   <span className="flex-grow flex flex-col">
@@ -50,14 +50,14 @@ export default async function Home() {
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
                 <p className="leading-relaxed mb-6">
-                  I apologize, but as of my last knowledge update in September
-                  2021, I do not have information about a designer named Alper
-                  Kamu in my database. It's possible that Alper Kamu is not
+                  I apologize but as of my last knowledge update in September
+                  2021 I do not have information about a designer named Alper
+                  Kamu in my database Its possible that Alper Kamu is not
                   widely known or that they gained prominence after my last
-                  update. If you could provide more context or specify the field
-                  or area of design in which Alper Kamu is active, I may be able
+                  update If you could provide more context or specify the field
+                  or area of design in which Alper Kamu is active I may be able
                   to provide more information or insights based on the
-                  information available up to that date.
+                  information available up to that date
                 </p>
                 <a className="inline-flex items-center">
                   <span className="flex-grow flex flex-col">
